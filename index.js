6c9f1dcca5e4c86407f71acb8649af6a88f35ab6
Megan Adams <meganoliviawendyadams@gmail.com>
2019-08-26 19:22:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,28 @@
 // Write your code here!
 
 
+<<<<<<< HEAD
 let element = document.querySelector("#main");
 element.remove("main");
 
 
-let newHeader = document.querySelector("h1#victory");
-newHeader.innerHTML = "<h1>Megan is the champion m8</h1>"
+let newHeader = document.querySelector("h1#victory")"
+=======
+let element = document.querySelector("p#greeting");
+element.innerHTML = 'Hello, DOM!'
+document.body.appendChild(element)
+var ul = document.createElement('ul')
+ 
+for (let i = 0; i < 3; i++) {
+  let li = document.createElement('li')
+  li.innerHTML = (i + 1).toString()
+  ul.appendChild(li)
+}
+ 
+element.appendChild(ul)
+
+element.innerHTML = 'Hello, DOM!'
+
+let header = document.getElementById("div#header");
+header.innerHTML = "<h1>Poodles!</h1><h3>An Essay into the Pom-Pom as Aesthetic Reconfiguration of the Other from a post-Frankfurt School Appropriationist Perspective</h3><p><em>By: Byron Q. Poodle, Esq., BA.</em></p>";
+>>>>>>> cddec00b393c3f654f4c96e3ccdad56f56a1fbef
